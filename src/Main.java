70cfc077dd96e4ffe3403ae4b0775571327fa9c7
camperdue42 <camperdue42@gmail.com>
2022-09-29 14:26:32 -0400

    Add maven repo class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        
+        MavenRepo repo = new MavenRepo();
+        repo.searchMaven();
     }
-    
 }
---
+++ src/MavenRepo.java
@@ -0,0 +1,6 @@
+public class MavenRepo {
+    public void searchMaven() {
+
+    }
+    
+}
