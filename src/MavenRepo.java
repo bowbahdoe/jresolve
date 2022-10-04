dcba880555eee25bde48def23c16290e08970743
Cameron <camperdue42@gmail.com>
2022-10-04 12:58:49 -0400

    change maven repo to use httpclient

--- src/MavenRepo.java
+++ src/MavenRepo.java
@@ -1,9 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
 
 /**
  * Class representing the Maven central repo, able to search the Maven central repository using the
@@ -16,6 +25,9 @@ import java.net.URL;
 public class MavenRepo {
 
     public void searchMaven() {
+        HttpClient client = null;
+        HttpRequest request = null;
+
         URL serverAddress = null;
         HttpURLConnection connection = null;
         BufferedReader reader = null;
@@ -23,31 +35,48 @@ public class MavenRepo {
         String line = null;
         
         try {
-            serverAddress = new URL("https://search.maven.org/solrsearch/select?q=guice&rows=20&wt=xml");
-            connection = null;
+            client = HttpClient.newBuilder()
+                .version(Version.HTTP_1_1)
+                .followRedirects(Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .build();
+            
+            request = HttpRequest.newBuilder()
+                .uri(URI.create("https://search.maven.org/solrsearch/select?q=guice&rows=20&wt=xml"))
+                .build();
 
-            connection = (HttpURLConnection) serverAddress.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoOutput(true);
-            connection.setReadTimeout(1000);
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
-            connection.connect();
+            System.out.println(response);
 
-            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            sb = new StringBuilder();
+            // serverAddress = new URL("https://search.maven.org/solrsearch/select?q=guice&rows=20&wt=xml");
+            // connection = null;
 
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + '\n');
-            }
+            // connection = (HttpURLConnection) serverAddress.openConnection();
+            // connection.setRequestMethod("GET");
+            // connection.setDoOutput(true);
+            // connection.setReadTimeout(1000);
 
-            System.out.println(sb.toString());
+            // connection.connect();
+
+            // reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            // sb = new StringBuilder();
+
+            // while ((line = reader.readLine()) != null) {
+            //     sb.append(line + '\n');
+            // }
+
+            // System.out.println(sb.toString());
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            connection.disconnect();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+        finally {
+            //connection.disconnect();
             reader = null;
             sb = null;
             connection = null;
